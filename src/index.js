66ad432dc5d334c1925c1fe31a13d484d4d267b2
Charlie <charlie.hoey@gmail.com>
2017-05-07 15:29:31 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import twgl from 'twgl.js';
+
+require('../css/main.scss');
+
+var el = document.getElementById('app');
+var canvas = document.createElement('canvas');
+el.appendChild(canvas);
+
+var gl = canvas.getContext("webgl");
+var programInfo = twgl.createProgramInfo(gl, [require('./shaders/default.vert'), require('./shaders/default.frag')]);
+
+var arrays = {
+  position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
+};
+var bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+
+function render(time) {
+  twgl.resizeCanvasToDisplaySize(gl.canvas);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  var uniforms = {
+    time: time * 0.001,
+    resolution: [gl.canvas.width, gl.canvas.height],
+  };
+
+  gl.useProgram(programInfo.program);
+  twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+  twgl.setUniforms(programInfo, uniforms);
+  twgl.drawBufferInfo(gl, bufferInfo);
+
+  requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var ClosureCompilerPlugin = require('google-closure-compiler-js').webpack;
+
+var PLUGINS = [];
+if (process.env.NODE_ENV === 'production') {
+  PLUGINS.push(    new ClosureCompilerPlugin({
+        compiler: {
+          language_in: 'ECMASCRIPT6',
+          language_out: 'ECMASCRIPT5',
+          compilation_level: 'SIMPLE'
+        },
+        concurrency: 3,
+      }))
+}
+
+module.exports = {
+  entry: ['whatwg-fetch','./src/index.js'],
+  output: {
+    path: __dirname,
+    filename: 'build.js'
+  },
+  plugins: PLUGINS,
+  module: {
+      rules: [{
+          test: /\.scss$/,
+          use: [{
+              loader: "style-loader" // creates style nodes from JS strings
+          }, {
+              loader: "css-loader" // translates CSS into CommonJS
+          }, {
+              loader: "sass-loader" // compiles Sass to CSS
+          }]
+      },
+      { test: /\.(glsl|frag|vert)$/, loader: 'raw-loader', exclude: /node_modules/ },
+      { test: /\.(glsl|frag|vert)$/, loader: 'glslify-loader', exclude: /node_modules/ }
+    ]}
+};
